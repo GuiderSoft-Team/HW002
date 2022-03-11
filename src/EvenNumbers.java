bc2d0c9e6e1a26afeb4827cb007f6e0b4de6dbf8
BİLAL <bllsfk.3984@gmail.com>
2022-03-11 17:56:33 +0300

    BİLAL ŞAFAK COMPLETED HIS HOMEWORK.

--- src/EvenNumbers.java
+++ src/EvenNumbers.java
@@ -1,7 +1,11 @@
 public class EvenNumbers {
     public static void main(String[] args) {
-        //TODO: Write a program that displays all even numbers between 1-50 in same line
-        //Each number should be separated by a space character
+
+        int sayi=0;
+        for (int i=0; i<50; i=i+2) {
+            sayi +=2;
+            System.out.print(sayi +"  ");
+        }
 
     }
 }
