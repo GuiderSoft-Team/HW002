ad168c43dd6719b93f10c61dfd259e8aedc6bd4c
LostMythic <ahmetselimozen35@gmail.com>
2022-03-11 12:59:40 +0100

    Ahmet Selimin Ödevi

--- src/TwoNumbers.java
+++ src/TwoNumbers.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class TwoNumbers {
     public static void main(String[] args) {
-        //TODO: Write a Java program that will accept two numbers and check if two numbers are equal or not.
+
         Scanner input=new Scanner(System.in);
         System.out.print("1. Sayi : ");
         int sayi1= input.nextInt();
