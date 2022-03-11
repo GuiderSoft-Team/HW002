ae725d13101f204c8770c8db340675e8fcc3e4d7
LostMythic <ahmetselimozen35@gmail.com>
2022-03-11 12:31:49 +0100

    Ahmet Selimin Çdevi homework 2

--- src/MultTable.java
+++ src/MultTable.java
@@ -1,6 +1,12 @@
+import static javax.swing.text.html.HTML.Attribute.N;
+
 public class MultTable {
     public static void main(String[] args) {
         //TODO: Write a Java program that prepares the multiplication tables from 1 to 10.
-
+        for (int i = 1; i <11;i++){
+            for( int j=1;j<11;j++){
+                System.out.println(i+" X "+j+" = "+(i*j));
+            }
+        }
     }
 }
--- src/TwoNumbers.java
+++ src/TwoNumbers.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
+
 public class TwoNumbers {
     public static void main(String[] args) {
         //TODO: Write a Java program that will accept two numbers and check if two numbers are equal or not.
+        Scanner input=new Scanner(System.in);
+        System.out.print("1. Sayi : ");
+        int sayi1= input.nextInt();
+        System.out.print("2. Sayi : ");
+        int sayi2= input.nextInt();
 
+        if (sayi1==sayi2){
+            System.out.println("Iki sayı eşittir.");
+        }else {
+            System.out.println("Iki sayi eşit değildir.");
+        }
     }
 }
