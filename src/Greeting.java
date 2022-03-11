aa5357bb63526bd3d0aaf34f5798693546f5cb54
nagehanceliksu <nagehanhceliksu@gmail.com>
2022-03-11 21:25:38 +0300

    NagehanCeliksu

--- src/Greeting.java
+++ src/Greeting.java
@@ -1,9 +1,19 @@
+import java.util.Scanner;
 public class Greeting {
     public static void main(String[] args) {
-        //TODO: Write a Java program to implement following logic using if statement
-        //1. if hour is less than 12 noon, greet with Good Morning
-        //2. if hour is greater than or equal 12 noon but less than 17, greet with Good Afternoon
-        //3. if hour is greater than or equal to 17, greet with Good Evening
+        System.out.println("Saati Giriniz : ");
+        Scanner input = new Scanner(System.in);
+        int saat = input.nextInt();
+        if (0 <= saat & saat < 12) {
+            System.out.println("Good Morning");
+        } else if (saat <= 12 & saat < 17) {
+            System.out.println("Good Afternoon ");
+        } else if (saat >= 17 & saat <= 24) {
+            System.out.println("Good Evening");
+        } else {
+            System.out.println("Lütfen geçerli bir saat giriniz.");
+        }
+    }
 
     }
-}
+
