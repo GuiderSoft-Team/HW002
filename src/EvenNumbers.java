2151fb522507171edd7c2c4fd3acf4a4fb83372f
erdalalioglu <erdalalioglu@gmail.com>
2022-03-11 02:08:11 +0100

    HomeWork02

--- src/EvenNumbers.java
+++ src/EvenNumbers.java
@@ -1,7 +1,13 @@
 public class EvenNumbers {
     public static void main(String[] args) {
-        //TODO: Write a program that displays all even numbers between 1-50 in same line
-        //Each number should be separated by a space character
 
-    }
-}
+                    for(int i=1;i<=50;i++){
+                        if (i%2==0)
+                    {
+                        System.out.print(i);
+                        System.out.print(" ");
+                    }
+                }
+
+            }
+        }
