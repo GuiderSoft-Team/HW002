b3f2e1cb5a872878b242eb7ec29fa3b7a64a8036
Yasin Ekin <ekinyasin90@gmail.com>
2022-03-11 14:58:02 +0300

    All requests were done

--- src/MultTable.java
+++ src/MultTable.java
@@ -1,6 +1,18 @@
+
+
 public class MultTable {
     public static void main(String[] args) {
-        //TODO: Write a Java program that prepares the multiplication tables from 1 to 10.
+
+        System.out.println("                   MULTIPLICATION TABLE                         ");
+        System.out.println("-----------------------------------------------------------------");
+
+        for(int i=1;i<=10;i++){
+            for(int j=1;j<=10;j++){
+
+                System.out.println(i+"x"+j+"="+(i*j)+" ");
+            }
+            System.out.println("--------------------------");
+        }
 
     }
 }
