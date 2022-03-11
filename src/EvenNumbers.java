6288e5db03c484a38976b51d0a4e7aeb691810db
HasanOzyer <hasanozyer06@gmail.com>
2022-03-11 16:38:03 +0300

    Hasan Özyer did his HomeWork...

--- src/EvenNumbers.java
+++ src/EvenNumbers.java
@@ -1,7 +1,38 @@
 public class EvenNumbers {
     public static void main(String[] args) {
-        //TODO: Write a program that displays all even numbers between 1-50 in same line
-        //Each number should be separated by a space character
 
+
+        for (int i=0; i <=50; i+=2 ) {
+
+            System.out.print("i: " + i + ", \t" );
+
+
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/Greeting.java
+++ src/Greeting.java
@@ -1,9 +1,35 @@
+import java.util.Scanner;
+
 public class Greeting {
     public static void main(String[] args) {
-        //TODO: Write a Java program to implement following logic using if statement
+
         //1. if hour is less than 12 noon, greet with Good Morning
         //2. if hour is greater than or equal 12 noon but less than 17, greet with Good Afternoon
         //3. if hour is greater than or equal to 17, greet with Good Evening
 
+        //Olumsuz durumlar için programı iyileştir.
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Welkam to time programme...");
+        System.out.println("************************************************************************************");
+
+        System.out.print("What time is it there dude (e.g. = 17,29) : ");
+        double time = input.nextDouble();
+
+
+
+        if (time < 12.00) {
+            System.out.println("Greetings and Good morning! My fellow American.");
+
+        }else if (time < 17.00) {
+            System.out.println("Good afternoon!?!");
+
+
+        }else if (time <= 24.00) {
+            System.out.println("Good eveninggggggg.");
+
+        }
     }
 }
+
