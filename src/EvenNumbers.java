addfc3d1d65baa279ee99942de94340aceebcbe0
erdalalioglu <erdalalioglu@gmail.com>
2022-03-12 12:33:04 +0100

    HomeWork02 Düzeltildi

--- src/EvenNumbers.java
+++ src/EvenNumbers.java
@@ -3,11 +3,9 @@ public class EvenNumbers {
 
                     for(int i=1;i<=50;i++){
                         if (i%2==0)
-                    {
-                        System.out.print(i);
-                        System.out.print(" ");
+
+                        System.out.print(i+" ");
                     }
-                }
 
             }
         }
