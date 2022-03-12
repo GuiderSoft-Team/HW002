8c55ffde955b1165306910c40fa047c1eac98e77
orhan374 <orhndgnr@gmail.com>
2022-03-13 00:48:24 +0300

    orhantokur tamamladı

--- src/EvenNumbers.java
+++ src/EvenNumbers.java
@@ -1,7 +1,16 @@
 public class EvenNumbers {
     public static void main(String[] args) {
-        //TODO: Write a program that displays all even numbers between 1-50 in same line
+        // Write a program that displays all even numbers between 1-50 in same line
         //Each number should be separated by a space character
+        int sayi;
+
+        for (sayi =1; sayi < 50; sayi++)
+        {
+            if(sayi%2==0) {
+                System.out.print(  "\t" );
+                System.out.print(+sayi);
+            }
+        }
 
     }
-}
+        }
