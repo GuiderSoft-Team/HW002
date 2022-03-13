3630a06cc388e5579127a2904da6d47fb9f04a11
Sadri <skamilfindikli@gmail.com>
2022-03-13 14:53:58 +0300

    Sadri ödev tamam...

--- src/EvenNumbers.java
+++ src/EvenNumbers.java
@@ -1,7 +1,16 @@
 public class EvenNumbers {
     public static void main(String[] args) {
-        //TODO: Write a program that displays all even numbers between 1-50 in same line
+
         //Each number should be separated by a space character
 
+        System.out.println("**1'den 50'ye Kadar Çift Sayılar**");
+        System.out.println("----------------------------");
+
+        for (int i =1; i <=50; i++) {
+            if(i%2==0)
+            System.out.print(i +" ");
+        }
+
+        }
     }
-}
+
--- src/Greeting.java
+++ src/Greeting.java
@@ -1,9 +1,36 @@
+import java.util.Scanner;
+
 public class Greeting {
     public static void main(String[] args) {
-        //TODO: Write a Java program to implement following logic using if statement
+
         //1. if hour is less than 12 noon, greet with Good Morning
         //2. if hour is greater than or equal 12 noon but less than 17, greet with Good Afternoon
         //3. if hour is greater than or equal to 17, greet with Good Evening
+                Scanner input=new Scanner(System.in);
 
+                System.out.print("Choose an hour a Meeting Clock:<00-24> :");
+                int clock= input.nextInt();
+
+                if(clock<12) {
+                    System.out.println("Good Morning!");
+                    clock=input.nextInt();
+                }else if(clock>=12&clock<17) {
+                    System.out.println("Good Afternoon!");
+                    clock=input.nextInt();
+                }else if(clock>=17&clock<20) {
+                    System.out.println("Good Evening!");
+                    clock=input.nextInt();
+                }else if(clock>20&clock<24){
+                    System.out.println("Good Night!");
+                    clock=input.nextInt();
+                }else{
+                    System.out.println("GEÇERSİZ SAAT DİLİMİ:"+clock);
+
+                }
+
+
+
+
+        }
     }
-}
+
