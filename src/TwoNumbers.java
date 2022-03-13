b2c01fdbd8d7e56df0fdfc9f3b364b6eff3e6a99
ibrahim ALTINOK <ibrhmaltnk98@gmail.com>
2022-03-13 18:12:48 +0300

    HW002 ÖDEV TESLİM

--- src/TwoNumbers.java
+++ src/TwoNumbers.java
@@ -1,6 +1,25 @@
+import java.util.Scanner;
+
 public class TwoNumbers {
     public static void main(String[] args) {
-        //TODO: Write a Java program that will accept two numbers and check if two numbers are equal or not.
+        // Write a Java program that will accept two numbers and check if two numbers are equal or not.
+
+        Scanner input=new Scanner(System.in);
+
+        System.out.print("lütfen 1. sayıyı giriniz="+"\t");
+        int sayi1= input.nextInt();
+
+        System.out.print("lütfen 2. sayıyı giriniz="+"\t");
+        int sayi2= input.nextInt();
+
+        if (sayi1 == sayi2) {
+            System.out.println("sayılar birbirine eşittir.");
+        }else {
+            System.out.println("sayılar birbirine eşit değildir.");
+        }
+
+
+
 
     }
 }
