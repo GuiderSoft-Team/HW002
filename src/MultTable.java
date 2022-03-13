018e36bf899f643945ff6ef0b15f59c6810ba0c6
EnesKula <100785101+EnesKula@users.noreply.github.com>
2022-03-13 15:10:20 +0300

    EneX ödevi yaptı.

--- src/MultTable.java
+++ src/MultTable.java
@@ -1,6 +1,11 @@
 public class MultTable {
     public static void main(String[] args) {
         //TODO: Write a Java program that prepares the multiplication tables from 1 to 10.
-
+        for (int a=1;a<=10;a++){
+            for (int b = 1; b <=10 ; b++) {
+                System.out.print(a+"*"+b+"="+a*b+" ");
+            }
+                System.out.println();
+        }
     }
 }
