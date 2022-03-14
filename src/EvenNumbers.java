fcc95f9fe6cbee9e68ad65576ed2f89a283b11a7
Zeynep Düzen <99576262+ZeynepDuzen@users.noreply.github.com>
2022-03-14 13:33:17 -0700

    HW2

--- src/EvenNumbers.java
+++ src/EvenNumbers.java
@@ -2,6 +2,11 @@ public class EvenNumbers {
     public static void main(String[] args) {
         //TODO: Write a program that displays all even numbers between 1-50 in same line
         //Each number should be separated by a space character
-
+        writeNumber(1,50);
+    }
+    public static void writeNumber(int ilkSayi, int ikinciSayi){
+        for (int i = ilkSayi; i <=ikinciSayi ; i++) {
+            System.out.print(i+" / ");
+        }
     }
 }
--- src/Greeting.java
+++ src/Greeting.java
@@ -4,6 +4,20 @@ public class Greeting {
         //1. if hour is less than 12 noon, greet with Good Morning
         //2. if hour is greater than or equal 12 noon but less than 17, greet with Good Afternoon
         //3. if hour is greater than or equal to 17, greet with Good Evening
+        greeting(8);
+        greeting(13);
+        greeting(21);
+
+    }
+    public static void greeting(int clock){
+        if (clock<12){
+            System.out.println("good morning");
+        }else if(clock>=12&clock<17) {
+            System.out.println("good afternoon");
+        }else if (clock>=17){
+            System.out.println("good evening");
+        }
+
 
     }
 }
