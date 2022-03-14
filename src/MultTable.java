7f7b33daa7af526de6010cd62ec06c364d428dd5
gazimurat <yemliha.cooker@gmail.com>
2022-03-14 22:25:40 +0300

    Gazimurat ödevini yaptı.

--- src/MultTable.java
+++ src/MultTable.java
@@ -1,6 +1,16 @@
 public class MultTable {
     public static void main(String[] args) {
-        //TODO: Write a Java program that prepares the multiplication tables from 1 to 10.
+        //Write a Java program that prepares the multiplication tables from 1 to 10.
+
+        for (int i = 0; i <= 10; i++) {
+            for (int j = 0; j <=10 ; j++) {
+                System.out.print(i+"x"+j+"="+(i*j)+"\t");
+
+            }
+            System.out.println();
+
+        }
+
 
     }
 }
--- src/TwoNumbers.java
+++ src/TwoNumbers.java
@@ -1,6 +1,21 @@
+import java.util.Scanner;
+
 public class TwoNumbers {
     public static void main(String[] args) {
-        //TODO: Write a Java program that will accept two numbers and check if two numbers are equal or not.
+        //Write a Java program that will accept two numbers and check if two numbers are equal or not.
 
+        Scanner input = new Scanner(System.in);
+        System.out.print("Birinci sayıyı giriniz: ");
+        int sayi1 = input.nextInt();
+
+        System.out.print("İkinci sayıyı giriniz: ");
+        int sayi2 = input.nextInt();
+
+        if (sayi1 == sayi2) {
+            System.out.println("İki sayı eşittir.");
+
+        } else {
+            System.out.println("İki sayı eşit değildir");
+        }
     }
 }
