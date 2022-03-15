f86aa434ae5aaa4198b431f748e80647249a80da
muhammedgny <muhammet.gny1907@gmail.com>
2022-03-15 17:11:44 +0300

    Muhammed Nur Güney ödev2

--- src/TwoNumbers.java
+++ src/TwoNumbers.java
@@ -1,6 +1,13 @@
 public class TwoNumbers {
     public static void main(String[] args) {
         //TODO: Write a Java program that will accept two numbers and check if two numbers are equal or not.
-
+        int sayı1 = 8;
+        int sayı2 = 8;
+        if (sayı1 == sayı2 ){
+            System.out.println("değerler bir birine eşit");
+        }
+        else {
+            System.out.println("değerler birbirine eşit değil");
+        }
     }
 }
