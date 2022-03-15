b6109d185671f2d63ed2db7653f563e3ad442b8f
muhammedgny <muhammet.gny1907@gmail.com>
2022-03-16 02:40:17 +0300

    Muhammed Nur Güney ödev2

--- src/MultTable.java
+++ src/MultTable.java
@@ -1,4 +1,6 @@
 public class MultTable {
+    private static Object t;
+
     public static void main(String[] args) {
         //TODO: Write a Java program that prepares the multiplication tables from 1 to 10.
         System.out.println("çarpım tablosu");
@@ -6,18 +8,19 @@ public class MultTable {
         int çarpım = 0;
 
         for (int i = 0; i <=10 ; i++) {
-            System.out.println(i+". tablo");
+            System.out.println(" ");
         for (int a = 0; a <= 10; a++) {
+            System.out.print( '\t');
             çarpım = a * sayı;
             System.out.print(a);
             System.out.print(" x ");
             System.out.print(sayı);
             System.out.print(" = ");
-            System.out.println(çarpım);
+            System.out.print(çarpım);
 
         }
             sayı++;
-            System.out.println(" ");
+            //System.out.print('\t');
 
         }
     }
