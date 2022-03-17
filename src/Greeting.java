73783f9809d155ab3e895d35566f3f5cd308c989
Gürhan Ustali <gurhanustali@gmail.com>
2022-03-17 18:47:57 +0300

    HW002 has been created. Geriden geliyorum. Kusura bakmayın.

--- src/Greeting.java
+++ src/Greeting.java
@@ -1,9 +1,29 @@
+import java.util.Scanner;
+
 public class Greeting {
     public static void main(String[] args) {
-        //TODO: Write a Java program to implement following logic using if statement
-        //1. if hour is less than 12 noon, greet with Good Morning
-        //2. if hour is greater than or equal 12 noon but less than 17, greet with Good Afternoon
-        //3. if hour is greater than or equal to 17, greet with Good Evening
+        Scanner input = new Scanner(System.in);
+        System.out.println("Lütfen saati giriniz");
+        {
+            int hour = (int) input.nextInt();
+            {
+                if (hour < 12) {
+                    System.out.println("GOOD MORNING!");
+                } else if (hour <= 12) {
+                    System.out.println("GOOD AFTERNOON!!");
 
-    }
-}
+                }
+                while  (hour >= 17) break; {
+
+                    System.out.println("GOOD AFTERNOON!!");
+
+                    // Write a Java program to implement following logic using if statement
+                    //1. if hour is less than 12 noon, greet with Good Morning
+                    //2. if hour is greater than or equal 12 noon but less than 17, greet with Good Afternoon
+                    //3. if hour is greater than or equal to 17, greet with Good Evening
+
+                }
+
+            }
+        }
+    }}
--- src/TwoNumbers.java
+++ src/TwoNumbers.java
@@ -1,6 +1,27 @@
+import java.util.Scanner;
+
 public class TwoNumbers {
     public static void main(String[] args) {
-        //TODO: Write a Java program that will accept two numbers and check if two numbers are equal or not.
+        //Write a Java program that will accept two numbers and check if two numbers are equal or not.
+        Scanner input=new Scanner(System.in); {
+        System.out.print("Lütfen iki sayı giriniz:"+"\t"); {
 
+
+
+            int number1=input.nextInt();
+                System.out.print("İkinci Sayı: ");
+
+                int number2=input.nextInt();
+
+                if(number1==number2){
+                         System.out.print("Sayılar eşittir.");}
+                   else {
+            System.out.print("Sayılar eşit değildir");}
+            }}
     }
 }
+
+
+
+
+
