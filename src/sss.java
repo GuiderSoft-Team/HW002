d5d350d534950918de38d7522bfc794509cfa2aa
orhan374 <orhndgnr@gmail.com>
2022-03-17 18:31:07 +0300

    orhantokur tamamladı çarpım tablosu

---
+++ src/sss.java
@@ -0,0 +1,9 @@
+public class sss {
+    public static void main(String[] args) {
+        System.out.println("75 -->"(convertFahreinheittoCelcius) "(grade:5)+"C);
+    }
+    public static double convertFahreinheittoCelcius ( int grade) {
+         double result=(grade-32)/1.8;
+         return  result;
+    }
+}
