85987805c9ee8acbfb1b016e02cfab34a5e42957
90551 <akarsu64engin@gmail.com>
2022-03-19 00:31:42 +0300

    homework 2 yapıldı

--- src/MultTable.java
+++ src/MultTable.java
@@ -1,6 +1,14 @@
 public class MultTable {
     public static void main(String[] args) {
-        //TODO: Write a Java program that prepares the multiplication tables from 1 to 10.
-
+        // Write a Java program that prepares the multiplication tables from 1 to 10.
+        System.out.println( "\t\t\t\t***ÇARPIM TABLOSU***");
+        for (int i = 1; i <=10; i++) {
+            for (int j = 1; j <=10; j++) {
+                System.out.print(i+"x"+j+"="+(i*j));
+                System.out.print("\t");
+            }
+            System.out.println();
+           // System.out.println("////////////////////////////////");
+        }
     }
 }
