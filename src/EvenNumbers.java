b5635e41c3559d612e33a5ae2c98510b0842c3d9
stokatlio2012 <stokatlio2012@gmail.com>
2022-05-11 22:20:39 +0300

    All project is finished.

--- src/EvenNumbers.java
+++ src/EvenNumbers.java
@@ -1,6 +1,5 @@
 public class EvenNumbers {
     public static void main(String[] args) {
-        //Each number should be separated by a space character
         for (int i=1;i<=50;i++){
             if(i%2==0){
                 System.out.print(i+" ");
--- src/MultTable.java
+++ src/MultTable.java
@@ -1,6 +1,12 @@
 public class MultTable {
     public static void main(String[] args) {
-        //TODO: Write a Java program that prepares the multiplication tables from 1 to 10.
+
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= 10; j++) {
+                System.out.print(j+" x "+i+" = "+i*j+"\t\t");
+            }
+            System.out.println();
+        }
 
     }
 }
