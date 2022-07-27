cdb9fe329077878ebdec0f60af4522ed23d25507
Mümin <mozdamar90@hotmail.com>
2022-07-27 19:43:34 +0300

    HW002 Ödevler Tamamlandı.

--- src/EvenNumbers.java
+++ src/EvenNumbers.java
@@ -3,5 +3,10 @@ public class EvenNumbers {
         //TODO: Write a program that displays all even numbers between 1-50 in same line
         //Each number should be separated by a space character
 
+        int ciftSayi=0;
+
+        for (int i = 0; i <=50 ; i=i+2)
+            System.out.print("\t," + i);
+
     }
 }
