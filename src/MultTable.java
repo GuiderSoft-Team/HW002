bc6e7a26d1f497dd74e9336ead19c8e1dbd379ab
ssüleyman <sdurmaz77@hotmail.com>
2022-07-27 11:39:47 +0300

    oldu

--- src/MultTable.java
+++ src/MultTable.java
@@ -1,6 +1,10 @@
 public class MultTable {
     public static void main(String[] args) {
         //TODO: Write a Java program that prepares the multiplication tables from 1 to 10.
-
+        int carpim = 1;
+        for (int i = 1; i <= 10; i++ ) {
+            carpim = carpim *i;
+            System.out.println("carpim= "+carpim );
+            }
+        }
     }
-}
--- src/TwoNumbers.java
+++ src/TwoNumbers.java
@@ -1,6 +1,19 @@
 public class TwoNumbers {
     public static void main(String[] args) {
         //TODO: Write a Java program that will accept two numbers and check if two numbers are equal or not.
+/*        int sayı2 = 19;
+        int sayı1=19;
+        if (sayı1 == sayı2) {
+            System.out.println("eşittir.");
+        } else {
 
-    }
-}
+            System.out.println("eşit değildir.");
+            int sayı1 = 17;
+            int sayı2 = 7;
+            if (sayı1 - sayı2 == 0) {
+
+                System.out.println("eşittir");
+            } else {
+                System.out.println("eşit degildir");*/
+            }
+        }
