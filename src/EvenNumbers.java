76ad5d2a6696d019d2016fdbbaa3f205a75798e2
nizamimtest <nizamimtest@gmail.com>
2022-07-29 13:54:50 +0400

    odev2

--- src/EvenNumbers.java
+++ src/EvenNumbers.java
@@ -1,6 +1,6 @@
 public class EvenNumbers {
     public static void main(String[] args) {
-        //TODO: Write a program that displays all even numbers between 1-50 in same line
+        //Write a program that displays all even numbers between 1-50 in same line
         //Each number should be separated by a space character
 
         for (int i = 2; i <=50 ; i=i+2){
