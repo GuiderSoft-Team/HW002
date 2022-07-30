a5c4d6a77988a7028b0a5653eeb19b9d75e97087
nizamimtest <nizamimtest@gmail.com>
2022-07-30 21:57:01 +0400

    nizamim

--- src/TwoNumbers.java
+++ src/TwoNumbers.java
@@ -1,6 +1,21 @@
+import java.util.Scanner;
+
 public class TwoNumbers {
     public static void main(String[] args) {
-        //TODO: Write a Java program that will accept two numbers and check if two numbers are equal or not.
+        //Write a Java program that will accept two numbers and check if two numbers are equal or not.
+            Scanner input=new Scanner(System.in);
+            System.out.print("1. Sayıyı Giriniz: ");
 
+            double bir=input.nextDouble();
+            System.out.print("2. Sayıyı Giriniz: ");
+            double iki=input.nextDouble();
+
+            if(bir>iki){
+                System.out.println("1. sayı büyüktür.");
+            }else if(iki>bir) {
+                System.out.println("2. sayı büyüktür.");
+            }else {
+                System.out.println("Sayillar eşittir.");
+            }
+        }
     }
-}
